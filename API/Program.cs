383871cad121ef9935fa53bd7ac61af34b5761aa
Petar <krstic8@gmail.com>
2023-05-03 16:20:53 +0200

    End of section 11

--- API/Program.cs
+++ API/Program.cs
@@ -17,7 +17,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 app.UseMiddleware<ExceptionMiddleware>();
 
-app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+app.UseCors(builder => builder.AllowAnyHeader().AllowCredentials().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.UseAuthentication();
 app.UseAuthorization();
